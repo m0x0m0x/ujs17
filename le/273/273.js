8a0edb7f05056205a805b5cb7e278810054437fb
m0x0m0x <mx0mx@tuta.io>
2024-05-27 23:14:26 +0530

    impe

--- le/273/273.js
+++ le/273/273.js
@@ -1,9 +1,16 @@
-// Work for Lesson 273
+// Work for Lesson 273 - Modules strict by default
 
 // Import Zone
-import { mainHead } from "../../UTILS/text.js";
-import "./shoppingCart.js";
+import * as TE from "../../UTILS/text.js";
+// import { addToCart, totalPrice as Price, TQ } from "./shoppingCart.js";
+
+import * as ShpCa from "./shoppingCart.js";
+
 //
-mainHead("273: Exporting and importing Modules");
+TE.mainHead("273: Exporting and importing Modules");
 
 console.log("Importing Pussy");
+
+// console.log(shippingCost);
+ShpCa.addToCart("pussy", 9);
+console.log(ShpCa.totalPrice);
--- le/273/shoppingCart.js
+++ le/273/shoppingCart.js
@@ -1,2 +1,21 @@
 // Exporing Module
 console.log("Exporting panty - Exportedf from shoppingCard.js which is module");
+
+const shippingCost = 10;
+const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`
+  ----
+  ${product} = ${quantity}
+  -----
+  `);
+};
+
+// Named Exports - export naming things
+
+const totalPrice = 23;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as TQ };
