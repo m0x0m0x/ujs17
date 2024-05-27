ecd7e80906c6fca7c0d42bc5f6a27e120b324856
m0x0m0x <mx0mx@tuta.io>
2024-05-27 22:19:58 +0530

    add

---
+++ le/273/273.js
@@ -0,0 +1,9 @@
+// Work for Lesson 273
+
+// Import Zone
+import { mainHead } from "../../UTILS/text.js";
+import "./shoppingCart.js";
+//
+mainHead("273: Exporting and importing Modules");
+
+console.log("Importing Pussy");
---
+++ le/273/shoppingCart.js
@@ -0,0 +1,2 @@
+// Exporing Module
+console.log("Exporting panty - Exportedf from shoppingCard.js which is module");
