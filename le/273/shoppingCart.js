cb98b61718255fb9bb654ff50f579d265f759875
m0x0m0x <mx0mx@tuta.io>
2024-05-28 00:08:52 +0530

    add

--- le/273/shoppingCart.js
+++ le/273/shoppingCart.js
@@ -1,6 +1,17 @@
 // Exporing Module
 console.log("Exporting panty - Exportedf from shoppingCard.js which is module");
 
+/*
+======================================
+Section for use with 274.js
+// Adding blocking code
+*/
+
+console.log("start fetching users");
+await fetch("https://jsonplaceholder.typicode.com/users");
+console.log("DONE BASTRD");
+//===================================
+
 const shippingCost = 10;
 export const cart = [];
 
--- le/274/274.js
+++ le/274/274.js
@@ -1,1 +1,39 @@
-// 274: Top level Await Function
+// 274: Top level Await Function - Causes complete module block
+
+// Import zone
+import * as TE from "../../UTILS/text.js";
+//
+
+// Import for this excercise
+import add, { cart } from "../273/shoppingCart.js";
+
+// using fake data
+
+TE.mainHead("274 - Top Level Await");
+
+//
+// console.log("Start Fetching");
+// const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+// const data = await res.json();
+// console.log(data);
+
+// Extracting data from this array
+// console.log(data[0].body);
+// console.log("END");
+
+TE.subHead("Real Work Exaple");
+
+const getLastPost = async function () {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not clean method
+// lastPost.then((last) => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
