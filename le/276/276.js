7689dda105a0cf56bf54a50e131dea92e2c68d42
m0x0m0x <mx0mx@tuta.io>
2024-05-28 06:25:46 +0530

    276

--- le/276/276.js
+++ le/276/276.js
@@ -1,1 +1,17 @@
-//276 - Common JS Modules
+/*
+276 - Common JS Modules - Traditional Method of JS modules
+*/
+
+// Export - This is the common JS
+// export.addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`
+//     ---
+//     ${product} = ${quantity}
+//     ---
+//     `);
+//   };
+
+// Import
+
+const { addToCart } = require("../273/shoppingCart.js");
