84460b14171c7db7ab3fe903302d5f93401ea106
m0x0m0x <mx0mx@tuta.io>
2024-05-28 05:51:25 +0530

    275

--- le/275/275.js
+++ le/275/275.js
@@ -1,1 +1,40 @@
 // 275 - Module Patter
+
+// IIFE - return data once
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`
+    ---
+    ${product} = ${quantity}
+    ---
+    `);
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`
+    ---
+    ${product} = ${quantity}
+    ---
+    ORDERED
+    `);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart("car", 4);
+ShoppingCart2.addToCart("bus", 5);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
