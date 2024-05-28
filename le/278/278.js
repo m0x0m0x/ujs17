380b58fa607613a505211782c1b68a41647ea5a6
m0x0m0x <mx0mx@tuta.io>
2024-05-28 12:45:05 +0530

    cloineDeep lodash

--- le/278/278.js
+++ le/278/278.js
@@ -4,3 +4,27 @@
 npm i lodash-es
 - Those functions will now be impored
 */
+
+// Import lodash
+/*
+clondeep - for copying nested objects
+*/
+import cloneDeep from "../../node_modules/lodash-es/cloneDeep.js";
+
+const state = {
+  cart: [
+    { product: "bus", quantity: 10 },
+    { product: "car", quantity: 5 },
+  ],
+  user: {
+    loggedIn: true,
+  },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
